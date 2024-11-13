753b8a829a91b9dd560f46168a1d5b9988f4703c
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-13 09:54:38 +0300

    Add Home page with introductory text

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Eco Home Guide</h1>
+      <p>Explore eco-friendly tips to make your home greener!</p>
+    </div>
+  );
+}
+
+export default Home;
