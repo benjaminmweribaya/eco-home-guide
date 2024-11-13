71f55e9390f48b73bb4e506314c98ce851df5836
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-13 09:58:52 +0300

    Integrate NavBar and routing for Home, Tips, and Favorites pages in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Tips from './pages/Tips';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/tips" component={Tips} />
+        <Route path="/favorites" component={Favorites} />
+      </Switch>
+    </Router>
   );
 }
 
