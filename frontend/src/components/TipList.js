34d6a0177db5e3e387d1326310ee65eb4b1f221f
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-13 12:26:15 +0300

    Implement favorite toggle button for each tip in TipList component.

--- frontend/src/components/TipList.js
+++ frontend/src/components/TipList.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import TipItem from './TipItem';
+import React, { useContext } from 'react';
+import { AppContext } from '../AppContext';
 
-function TipList({ tips, onFavorite, onComplete }) {
-  return (
-    <div className="tip-list">
-      {tips.map((tip) => (
-        <TipItem key={tip.id} tip={tip} onFavorite={onFavorite} onComplete={onComplete} />
-      ))}
-    </div>
-  );
+function TipList({ tips }) {
+    const { toggleFavorite } = useContext(AppContext);
+
+    return (
+        <div className="tip-list">
+            {tips.map((tip) => (
+                <div key={tip.id} className="tip-item">
+                    <p>{tip.text}</p>
+                    <button onClick={() => toggleFavorite(tip.id)}>
+                        {tip.isFavorite ? 'Unfavorite' : 'Favorite'}
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default TipList;
