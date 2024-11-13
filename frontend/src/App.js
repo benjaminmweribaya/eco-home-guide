0f6299c0d1b727dc5dae9a8f9f486d5c1dd86a77
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-13 11:02:16 +0300

    Wrap App with AppProvider for global state management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,22 @@ import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Tips from './pages/Tips';
 import Favorites from './pages/Favorites';
+import { AppProvider } from "./AppContext";
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tips" element={<Tips />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </Router>
+    <AppProvider>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tips" element={<Tips />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
+      </Router>
+    </AppProvider>
   );
 }
 
