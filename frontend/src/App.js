307e1db642b151efe9496e552f043d805a9ffad9
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-13 10:05:50 +0300

    Refactor App component to use Routes instead of Switch for react-router-dom v6 compatibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Tips from './pages/Tips';
@@ -9,11 +9,11 @@ function App() {
   return (
     <Router>
       <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/tips" component={Tips} />
-        <Route path="/favorites" component={Favorites} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tips" element={<Tips />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </Router>
   );
 }
