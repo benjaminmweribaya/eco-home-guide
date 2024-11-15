2971092f8c18445dc08793b4c1daf7f45d6afd93
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-15 12:25:32 +0300

    update home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="home-container text-center py-6 max-w-screen-md mx-auto">
             <h1 className="text-3xl font-bold text-green-700 mb-4">Welcome to the Eco Home Guide</h1>
             <p className="text-gray-700 mb-6">Explore eco-friendly practices to make your home more sustainable.</p>
-            <h3 className="text-xl font-semibold text-green-600 mb-2">Categories:</h3>
+            <h3 className="text-xl font-semibold text-green-600 mb-2">Categories</h3>
             <ul className="list-disc list-inside text-gray-800">
                 {categories.map((category, index) => (
                     <li key={category.id}>{index + 1}. {category.name}</li>
